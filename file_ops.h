a2beaa2342b22bb4960bb1c51cafee69d8bc7ad6
Rupali Rana <rupalirana.21034@gmail.com>
2025-10-04 21:24:03 +0530

    Added structure and file operation modules for PBL

---
+++ file_ops.h
@@ -0,0 +1,14 @@
+#ifndef FILE_OPS_H
+#define FILE_OPS_H
+#include "structure.h"
+#include "folder.h"
+
+// File operations
+FSFile* file_new(const char* name, FOLDER* parent, size_t size);
+int file_delete(FOLDER* folder, const char* name);
+int file_rename(FOLDER* folder, const char* old_name, const char* new_name);
+int file_move(FSFile* file, FOLDER* new_parent);
+void file_info(FSFile* file);
+void file_list(FOLDER* folder);
+
+#endif
